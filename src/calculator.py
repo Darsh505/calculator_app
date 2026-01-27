2acfffeb7171bed6c3f8fd8947776da83752da93
Darsh505 <darshveersahota@gmail.com>
2026-01-27 11:44:56 +0530

    add basic calculator logic

--- src/calculator.py
+++ src/calculator.py
@@ -1,5 +1,26 @@
+class calculator:
+    def evaluate(self, expression):
+        try:
+            result = eval(expression)
+            return result
+
+        except ZeroDivisionError:
+            return "error: division by zero not possible"
+        except Exception:
+            return "invalid expression"
+
 def main():
-    print("calculator app started successfully!")
+    calc = calculator()
+
+    while True:
+        expression = input("Enter an expression:")
+
+        if expression.lower() == "exit":
+            print("goodbye!")
+            break
+
+        result = calc.evaluate(expression)
+        print("result:", result)
 
 if __name__ == "__main__":
-    main()    
+    main()            
